5c1d68cb4cb10aad1362d57170e21df2313acf8a
almog itskovich <almogitskovich@gmail.com>
2022-09-15 20:13:59 +0300

    btn

--- CountDown/app.js
+++ CountDown/app.js
@@ -4,25 +4,24 @@ function randomDate(start, end) {
  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
 };
 const futureDate=randomDate(new Date(2022,9,10),new Date(2023,1,1));
-//print future date
-//console.log("future" +" "+ futureDate.getTime()); 
-//print expire date to the screen 
-const  tempToday= new Date().getTime();
-const year=futureDate.getFullYear()
-const month=futureDate.getMonth()+1;
-const day=futureDate.getDay()+1;
+//print expire date to the screen :
+const year=futureDate.getFullYear();
+const month=String(futureDate.getMonth() + 1).padStart(2, '0');
+const day=String(futureDate.getDate()).padStart(2, '0');
 document.querySelector(".expire-date").innerHTML=`the sale will begin at :${ day+ "/"+ month +"/"+year}`;
 //counting down func:
 const countDown=()=>{
     const today= new Date().getTime();
-    // console.log("today"+" " +today);
-    const counter=futureDate.getTime()-today;
-    // console.log("counter"+" "+counter);
+    //  const counter=futureDate.getTime()-today;
+    const counter=today-today;
          if(counter<0){
          clearInterval(x);
         }
         if(counter===0){
-            ShowBth();
+            b();
+            //  ShowBth(); 
+            clearInterval(x);
+            // removeAttr();
         }
     //convert into year month hour from mil sec to "normal"
     const second=1000;
@@ -34,7 +33,7 @@ const countDown=()=>{
     const hourcounter=Math.floor((counter%day)/hour);
     const minutecounter=Math.floor((counter%hour)/minute);
     const secoundcounter=Math.floor((counter%minute)/second);
-   //to html file
+   //to html file:
    document.querySelector('.days').innerHTML=daycounter;
    document.querySelector('.hours').innerHTML=hourcounter;
    document.querySelector('.minutes').innerHTML=minutecounter;
@@ -45,7 +44,7 @@ const countDown=()=>{
 let x =setInterval(function(){
 countDown();
 },1000);
-    //-----------images-------------
+    //-----------images-----------------
 const images=new Array(20);         
 //function that puts the pictures into an fixed array maybe dosent needed! 
 let a =()=>{
@@ -60,32 +59,56 @@ console.log(images);
 const imgAt=()=>{
 var index=Math.ceil(Math.random()*20);
 
-   var htmltag=`<img src="/images/p${index}.jpg">`;
+   let htmltag=`<img src="/images/p${index}.jpg">`;
    console.log(htmltag);
 
     document.querySelector('.product-img').innerHTML=htmltag;
 }
 imgAt();
-//----------btn functions------------------
-let hidden=true; 
-var ShowBth=()=>{
-   hidden=!hidden;
-   if(hidden===false){
-     document.querySelector('.btn btn-outline-primary').style.visibility = 'visible';
-   }
+
+//----------btn function------------------
+const b=()=>{
+document.querySelector(".btn-click-to-buy").innerHTML=`<button type="button" class="btn btn-outline-primary" onclick="ShowBth()">  the sale begins now
+</button>`;
+
+};
 
 
+//when the counter is 0 0 0 0 the btn shows
+ let hidden=true;
+const ShowBth=()=>{
+    hidden=!hidden;
+    if(hidden===false){
+    paypal.Buttons({
+        style : {
+            size:'mini',
+            color: 'black',
+            shape: 'pill'
+        },
+        createOrder: function (data, actions) {
+            return actions.order.create({
+                purchase_units : [{
+                    amount: {
+                        currency_code:'USD',
+                        value: '0.1'
+                    }
+                }]
+            });
+        },
+        onApprove: function (data, actions) {
+            return actions.order.capture().then(function(details) {
+                console.log(details)
+                window.location.replace("")
+            })
+        },
+        onCancel: function (data) {
+            window.location.replace("")
+        }
+        }).render('.btn-click-to-buy');
+      
+   };
+    hidden=!hidden;
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
